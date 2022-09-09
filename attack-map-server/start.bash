972879bf9127dd696af8c0132494b71ab4effd7d
seubpong.mon <pjame.fb@gmail.com>
2022-09-09 08:55:25 +0700

    Added configuration via env variables

--- attack-map-server/start.bash
+++ attack-map-server/start.bash
@@ -5,4 +5,9 @@ sed -i "s#__GEOIP_LAT__#${GEOIP_LAT}#g" ${OUTPUT_FILE}
 sed -i "s#__GEOIP_LONG__#${GEOIP_LONG}#g" ${OUTPUT_FILE}
 sed -i "s#__GEOIP_MAPBOX_TOKEN__#${GEOIP_MAPBOX_TOKEN}#g" ${OUTPUT_FILE}
 
+OUTPUT_FILE=static/index.html
+sed -i "s#__GEOIP_LEGEND1__#${GEOIP_LEGEND1}#g" ${OUTPUT_FILE}
+sed -i "s#__GEOIP_LEGEND2__#${GEOIP_LEGEND2}#g" ${OUTPUT_FILE}
+sed -i "s#__GEOIP_IMAGE_PATH__#${GEOIP_IMAGE_PATH}#g" ${OUTPUT_FILE}
+
 python3 -u AttackMapServer.py
